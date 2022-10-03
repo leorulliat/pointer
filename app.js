2d85f64e2fed0df056c994aefb203f5efc55ed92
Léo Rulliat <leorulliat@gmail.com>
2022-10-03 21:19:09 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+})
+
+app.listen(PORT, () => console.log(`Server started on port : ${PORT}`));
